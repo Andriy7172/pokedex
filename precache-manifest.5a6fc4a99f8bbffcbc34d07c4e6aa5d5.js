907b441d4c2051a8e1fc5f3a3dde5d9ac90df898
Andriy7172 <andriyko.zoh5@gmail.com>
2019-05-09 19:56:48 +0300

    Updates

---
+++ precache-manifest.5a6fc4a99f8bbffcbc34d07c4e6aa5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c9a44ef2ac79558716be3d0f673af1",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c925cf299c6902103075",
+    "url": "/pokedex/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b94cb63d5d01034b2a17",
+    "url": "/pokedex/static/css/main.8342fa10.chunk.css"
+  },
+  {
+    "revision": "c925cf299c6902103075",
+    "url": "/pokedex/static/js/2.38c51cd0.chunk.js"
+  },
+  {
+    "revision": "b94cb63d5d01034b2a17",
+    "url": "/pokedex/static/js/main.ff6ac7de.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
